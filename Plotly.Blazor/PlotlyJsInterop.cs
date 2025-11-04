829be18bf54a2a7c88eb490fae9fd4842e0d8f61
Vladimir <vladimir.martynovskyy@cognyte.com>
2025-11-04 09:04:30 +0200

    Update with custom Plotly.js 3.2.0

--- Plotly.Blazor/PlotlyJsInterop.cs
+++ Plotly.Blazor/PlotlyJsInterop.cs
@@ -15,8 +15,8 @@ namespace Plotly.Blazor;
 public class PlotlyJsInterop
 {
     private const string InteropPath = "./_content/Plotly.Blazor/plotly-interop-6.0.1.js";
-    private const string PlotlyPath = "./_content/Plotly.Blazor/plotly-3.0.0.min.js";
-    private const string PlotlyBasicPath = "./_content/Plotly.Blazor/plotly-basic-3.0.0.min.js";
+    private const string PlotlyPath = "./_content/Plotly.Blazor/plotly-3.2.0.min.js";
+    private const string PlotlyBasicPath = "./_content/Plotly.Blazor/plotly-basic-3.2.0.min.js";
 
     private readonly DotNetObjectReference<PlotlyChart> dotNetObj;
     private readonly Lazy<Task<IJSObjectReference>> moduleTask;
